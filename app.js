282264c148dff7ee48c26e2272d84daf6d7c7e0c
Pastafarista <antoniolandin@proton.me>
2024-03-20 09:58:40 +0100

    Añadido app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+// Creamos la aplicación express
+const app = express()
+
+// Le decimos a la aplicación que user cors para evitar el error Corss-Domain (XD)
+app.use(cors())
+app.use(express.json())
+
+// El puerto es obtenido desde las variables de entorno, si no existe, se usa el puerto 3000
+const port = process.env.PORT ?? 3000
+
+// Escuchamos en el puerto
+app.listen(port, () => {
+    console.log(`Servidor escuchando en el puerto ${port}`)
+})
