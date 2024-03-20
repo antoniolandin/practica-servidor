524a4924502d07fe89256db3b35a4b23e30724e4
Pastafarista <antoniolandin@proton.me>
2024-03-20 10:12:29 +0100

    Feat: conexión a la base de datos

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.use(express.json())
 // El puerto es obtenido desde las variables de entorno, si no existe, se usa el puerto 3000
 const port = process.env.PORT ?? 3000
 
+// Conectamos a la base de datos
+const dbConnect = require('./config/mongo')
+dbConnect()
+
 // Escuchamos en el puerto
 app.listen(port, () => {
     console.log(`Servidor escuchando en el puerto ${port}`)
--- config/mongo.js
+++ config/mongo.js
@@ -1,1 +1,24 @@
+const mongoose = require('mongoose');
+
+const dbConnect = () => {
+
+    // Obtenemos la URI de la base de datos desde las variables de entorno
+    const db_uri = process.env.DB_URI;
+
+    // Evitar warnings de mongoose
+    mongoose.set('strictQuery', false);
+    
+    // Intentamos conectar a la base de datos
+    try {
+        mongoose.connect(db_uri)
+    }
+    catch (error) {
+        console.err(`Error al conectar a la base de datos: ${error}`);
+    }
+    
+    // Eventos de conexión
+    mongoose.connection.on("connected", () => { console.log("Conectado a la base de datos") });
+}
+
+module.exports = dbConnect;
 
